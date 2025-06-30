2d6e7a1deca1ea710c47adaa78925e568af76ce7
JuanJose <juaan.josehernandez@gmail.com>
2025-06-30 14:53:43 -0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,41 @@
-let form = document.getElementById("form");
-let buttonRegister = document.getElementById("buttonRegister-container")
+const users = {
+    0: {
+        userEmail: "admin@admin.com",
+        userPassword: "admin123",
+        rol:"admin"
+    },
+    
+ /*
+    Ejemplo
+    1: { 
+        userName: "Sofia",
+        userLastName: "Lopera",
+        userEmail: "sofia@gmail.com",
+        userPassword: "sofia123",
+        userNumber: "3212321231",
+        userCountry: "Colombia",
+        userCity: "Medellín",
+        userAddress: "Crra 65B #117-19",
+        userPostcode: "0005552",
+        role:"user"
+    }
 
-form.addEventListener("submit", (event)=>{
+    */
+}
 
+
+function registerUser(info) {
+    users[info[0]] = {userName:info[1], UserLastName:info[2], userEmail:info[3], userPassword: info[4], userNumber: info[5], userCountry: info[6], userCity: info[7], userAddres: info[8], userPostcode: info[9], role: info[10]}
+}
+
+
+//Vars of DOM
+const form = document.getElementById("form");
+const btnRegister = document.getElementById("btnRegister");
+
+
+//Validate login
+form.addEventListener("submit",(event)=>{
     event.preventDefault();
 
     let email = document.getElementById("email").value;
@@ -19,9 +52,7 @@ form.addEventListener("submit", (event)=>{
     }; 
 });
 
-function openRegisterPage() {
-
-    setTimeout(() => {
-        window.location.href = "../pages/register.html"
-    }, 1000); // 1000 milisegundos = 1 segundo
+export {
+    registerUser,
+    users
 }
